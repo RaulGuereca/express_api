60f9260add8a1d4eb1b80ba6e0910c614d855b73
RaulGuereca <launchx10296@innovaccion.mx>
2022-04-28 22:50:00 -0500

    Realizando ejercicios con endpoints con get post y put

---
+++ app.js
@@ -0,0 +1,45 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// esto es un endpoint
+//http methods, get, post, put, delete
+app.get('/v1/explorers', (req, res) => {
+    console.log('Api Explorers GET ALL requests ${new Date()}')
+    const explorer1 = {id: 1, name: "Carlo1"}
+    const explorer2 = {id: 2, name: "Carlo2"}
+    const explorer3 = {id: 3, name: "Carlo3"}
+    const explorer4 = {id: 4, name: "Carlo4"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log('Api Explorers GET request ${new Date()}')
+    console.log('Getting explorer with id ${req.param.id}')
+    const explorer1 = {id: 1, name: "Carlo1"}
+    res.status(200).json(explorer1)
+})
+
+app.post('/v1/explorers', (req, res) => {
+    console.log('Api Explorers POST request ${new Date()}')
+    const requestBody = req.body // parametros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log('Api Explorers PUT request ${new Date()}')
+    console.log('Update explorer with id ${req.param.id}')
+    const requestBody = req.body
+    res.status(200).json({message: "Updated!!"})
+})
+
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
